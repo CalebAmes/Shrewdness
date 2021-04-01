f5ab164dc15fa84b32f36d5bedb9ce88b55bed11
Caleb <caleb-gilbert@live.com>
2021-04-01 16:14:14 -0400

    home page shows user, csrf disabled

--- electron/src/App.js
+++ electron/src/App.js
@@ -7,6 +7,7 @@ import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import Home from './components/Home';
 import Users from "./components/Users";
+import Groups from "./components/Groups";
 import { getChannelMessages } from './store/channelMessages';
 import { getChannel } from './store/channels';
 import { getDirectMessages } from './store/directMessages';
@@ -42,6 +43,9 @@ function App() {
           </Route>
           <Route path= '/users' >
             <Users />
+          </Route>
+          <Route path= '/groups' >
+            <Groups />
           </Route>
           <Route path='/' exact >
             <Home />
