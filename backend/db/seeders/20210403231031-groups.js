cbedff9da200a4c7b8c152a74c82c611ed9eaef7
Caleb <caleb-gilbert@live.com>
2021-04-03 19:13:11 -0400

    models, migrations, seeders all regenerated in the CORRECT order

---
+++ backend/db/seeders/20210403231031-groups.js
@@ -0,0 +1,51 @@
+'use strict';
+const faker = require('faker');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Groups',
+      [
+        {
+          name: 'Gorillas',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+        {
+          name: 'Chimpanzees',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+        {
+          name: 'Origin Club',
+          avatar: faker.image.cats(),
+          description: `This is a Private Group for only the Shrewdest. If you don't know how to get in, you can't get in.`,
+        },
+        {
+          name: 'Marmosets',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+        {
+          name: 'Papionini',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+        {
+          name: 'Macaque',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+        {
+          name: 'Baboons',
+          avatar: faker.image.cats(),
+          description: faker.lorem.sentence(2),
+        },
+      ]
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Groups')
+  }
+};
