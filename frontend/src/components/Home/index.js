8f024505aa4c4aa24868def0431716239eab9db4
Caleb <caleb-gilbert@live.com>
2021-04-07 23:15:56 -0400

    splash done

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -27,7 +27,22 @@ const Home = () => {
         <Image className='image imageTen'/>
         <div className='yourText'>
           <h1>Your place to talk</h1>
-          <h3>Whether you're part of a school club, gaming group, worldwide art community, or just a handful of friends that want to spend some time together, Shrewdness makes it easy to talk every day and hang out more often.</h3>
+          <h3>Whether you're part of a school club, gaming group,
+             worldwide art community, or just a handful of friends that want to spend some time together, Shrewdness makes it easy to talk every day and hang out more often.</h3>
+        </div>
+      </div>
+      <div className='plentyBlock'>
+        <div className='plentyText'>
+          <h1>A place with plenty of room to talk</h1>
+          <h3>Shrewdness groups are organized into topic-based
+            channels where you can collaborate, share, and just 
+            talk about your day without clogging up a group chat.</h3>
+        </div>
+      </div>
+      <div className='journeyBlock'>
+        <div className='journeyText'>
+          <h1>Ready to start your journey?</h1>
+          <h3>What are you waiting for? Start using Shrewdness today.</h3>
         </div>
       </div>
     </>
