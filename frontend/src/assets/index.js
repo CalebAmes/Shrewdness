8eb82ac83334c0dd5d52fd68fb093fd77e8eca5b
Caleb <caleb-gilbert@live.com>
2021-04-18 13:25:04 -0400

    little extra organization

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,17 +1,36 @@
+// this is for the Electron version of this application
+// import { ipcRenderer } from 'electron';
+
+
 export const main = () => {
 	const body = document.getElementById('body');
 	body.className = '';
 	body.classList.add('main');
+	// this is for the Electron version of this application
+	// ipcRenderer.send('SAVE_USER_THEME', {
+  //   theme: 'main'
+  // });
+
 };
 
 export const darkmode = () => {
 	const body = document.getElementById('body');
 	body.className = '';
 	body.classList.add('darkmode');
+		// this is for the Electron version of this application
+	// ipcRenderer.send('SAVE_USER_THEME', {
+	// 	theme: 'darkmode'
+	// });
+
 };
 
 export const blue = () => {
 	const body = document.getElementById('body');
 	body.className = '';
 	body.classList.add('blue');
+		// this is for the Electron version of this application
+	// ipcRenderer.send('SAVE_USER_THEME', {
+	// 	theme: 'blue'
+	// });
+
 };
