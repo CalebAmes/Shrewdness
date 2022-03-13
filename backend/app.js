3077214869d063453cbab173820be6e9fd40d969
Caleb <clutchcaleb@gmail.com>
2022-03-13 14:52:36 -0400

    cors updata

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (!isProduction) {
-	app.use(cors());
+	app.use(cors({ origin: 'http://localhost:3000/' }));
 }
 
 app.use(
